bb7f78f7e3aafa7762d1102faee99bfe5525f840
luminecs <wangxuke.china@gmail.com>
2024-11-28 16:46:18 +0800

    commit

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ func main() {
 
 	// Register the routes and handlers
 	mux.Handle("/", &homeHandler{})
+	mux.Handle("/recipes", &RecipesHandler{})
+	mux.Handle("/recipes/", &RecipesHandler{})
 
 	// Run the server
 	http.ListenAndServe(":8080", mux)
@@ -24,6 +26,6 @@ func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 type RecipesHandler struct {
 }
 
-func (h RecipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *RecipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("This is my recipe page"))
 }
